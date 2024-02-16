ce84c5ee63db88622ebc0df0877520bad8a626c0
anirbit16 <88415990+anirbit16@users.noreply.github.com>
2024-02-16 06:48:05 +0000

    02_basics committed

---
+++ 02_basic.js
@@ -0,0 +1,20 @@
+"use strict"//treat all JS code as newer version
+
+// alert("hello")
+// alert( 3 + 3) We are using nodejs not browser
+// console.log(3+3)
+
+// let name = "Anirbit"
+// let age = 4
+// let isLoggedIn = false
+// number = 2^53
+// string = ""
+//boolean => true/false
+// null = standalone value
+// undefined =>
+//symbol =>  unique
+//object-
+console.log(typeof("Anirbit"))
+console.log(typeof undefined)
+console.log(typeof null) 
+//null is an object
